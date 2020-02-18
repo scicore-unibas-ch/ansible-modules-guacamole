142edd81a717d0d9f29c85e6142e65b720bb5364
Pablo Escobar <pescobar001@gmail.com>
2020-02-18 10:59:24 +0100

    clenar function get_token()

--- plugins/module_utils/guacamole.py
+++ plugins/module_utils/guacamole.py
@@ -8,7 +8,7 @@ import json
 from ansible.module_utils.urls import open_url
 from ansible.module_utils.six.moves.urllib.parse import urlencode
 
-
+URL_GET_TOKEN = "{url}/api/tokens"
 URL_LIST_CONNECTIONS = "{url}/api/session/data/{datasource}/connectionGroups/\
 {parent_identifier}/tree?token={token}"
 
@@ -18,37 +18,49 @@ class GuacamoleError(Exception):
 
 
 def guacamole_get_token(base_url, validate_certs, auth_username, auth_password):
+    """
+    Retun a dict with a token to authenticate with the API and a datasource.
+    DataSource can be "postgresql" or "mysql" depending on how guacamole is configured.
 
-    url_token = base_url + "/api/tokens"
+    Example of what this function returns:
+    {
+        'authToken': 'AAAAABBBBBCCCCCDDDDD",
+        'dataSource': 'postgresql'
+    }
+    """
+
+    url_get_token = URL_GET_TOKEN.format(url=base_url)
+
     payload = {
         'username': auth_username,
         'password': auth_password
     }
+
     try:
-        r = json.load(open_url(url_token, method='POST',
+        token = json.load(open_url(url_token, method='POST',
                                validate_certs=validate_certs,
                                data=urlencode(payload)))
     except ValueError as e:
         raise GuacamoleError(
             'API returned invalid JSON when trying to obtain access token from %s: %s'
-            % (url_token, str(e)))
+            % (url_get_token, str(e)))
     except Exception as e:
         raise GuacamoleError('Could not obtain access token from %s: %s'
-                             % (url_token, str(e)))
+                             % (url_get_token, str(e)))
     try:
         return {
-            'authToken': r['authToken'],
-            'dataSource': r['dataSource'],
+            'authToken': token['authToken'],
+            'dataSource': token['dataSource'],
         }
     except KeyError:
         raise GuacamoleError(
-            'Could not obtain access token from %s' % url_token)
+            'Could not obtain access token from %s' % url_get_token)
 
 
 def guacamole_get_connections(base_url, validate_certs, datasource, parent_identifier, auth_token):
     """
     Return a list of dicts with all the connections registered in the guacamole server
-    for the provided parent_identifier
+    for the provided parent_identifier. Default parent_identifier is ROOT
     """
 
     url_list_connections = URL_LIST_CONNECTIONS.format(
