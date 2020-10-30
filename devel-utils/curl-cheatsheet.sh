26e555edddfd5750b6a966516f8ec59e5eb617f5
Pablo Escobar <pescobar001@gmail.com>
2020-10-30 13:13:34 +0100

    added docker-compose.yml and curl examples

---
+++ devel-utils/curl-cheatsheet.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+guacamole_url="http://localhost:8080/guacamole"
+guacamole_user="guacadmin"
+guacamole_password="guacadmin"
+#echo ${guacamole_url}
+
+token_json=$(curl -s -X POST -H "Content-Type: application/x-www-form-urlencoded" -d "username=${guacamole_user}&password=${guacamole_password}" ${guacamole_url}/api/tokens)
+
+guacamole_token=$(echo $token_json | jq -r '.authToken')
+echo $guacamole_token
+guacamole_datasource=$(echo $token_json | jq -r '.dataSource')
+echo $guacamole_datasource
+
+guacamole_connections_urls="${guacamole_url}/api/session/data/${guacamole_datasource}/connectionGroups/ROOT/tree?token=${guacamole_token}"
+curl -s $guacamole_connections_urls | jq -C .
+
+guacamole_connections_groups_url="${guacamole_url}/api/session/data/${guacamole_datasource}/connectionGroups/?token=${guacamole_token}"
+curl -s $guacamole_connections_groups_url | jq -C .
