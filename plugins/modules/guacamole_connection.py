8f2e33b9fac3a431f16659eabcf0657f9500529f
paxprot <30484122+paxprot@users.noreply.github.com>
2022-10-13 22:17:46 +0800

    Update guacamole_connection.py

--- plugins/modules/guacamole_connection.py
+++ plugins/modules/guacamole_connection.py
@@ -171,6 +171,15 @@ options:
             - sv-se-qwerty
             - tr-tr-qwerty
             - failsafe
+    rdp_width:
+        description:
+            - Display width
+        type: int
+        
+    rdp_height:
+        description:
+            - Display height
+        type: int
 
     ssh_passphrase:
         description:
@@ -420,6 +429,8 @@ def guacamole_populate_connection_payload(module_params):
             "enable_full_window_drag",
             "security",
             "server_layout",
+            "width",
+            "height"
         )
         guacamole_add_parameter(payload, module_params, parameters, "rdp")
         if module_params.get('rdp_ignore_server_certs'):
@@ -528,6 +539,8 @@ def main():
                 'failsafe',
             )
         ),
+        rdp_width=dict(type='int'),
+        rdp_height=dict(type='int'),
         state=dict(type='str', choices=['absent', 'present'], default='present'),
         max_connections=dict(type='int', default=1),
         max_connections_per_user=dict(type='int'),
